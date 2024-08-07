e9489c5434ba894236ec2609f86ab97245771f0b
nusud <buniakesteban@gmail.com>
2024-08-07 18:01:00 -0300

    Add models for post and user

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-posts = [
-    {
-        'author': 'Esteban Buniak',
-        'title': '1st post',
-        'content': 'content for the 1st post',
-        'date_posted': 'August 7, 2024'
-    },
-    {
-        'author': 'Esteban Buniak',
-        'title': '2nd post',
-        'content': 'content for the 2nd post',
-        'date_posted': 'August 7, 2024'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
