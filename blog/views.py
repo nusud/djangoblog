54d98c8013dd041af34ed0407165d6b1e81eb9f0
nusud <buniakesteban@gmail.com>
2024-08-07 15:16:29 -0300

    Add templates and bootstrap styles

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+posts = [
+    {
+        'author': 'Esteban Buniak',
+        'title': '1st post',
+        'content': 'content for the 1st post',
+        'date_posted': 'August 7, 2024'
+    },
+    {
+        'author': 'Esteban Buniak',
+        'title': '2nd post',
+        'content': 'content for the 2nd post',
+        'date_posted': 'August 7, 2024'
+    }
+]
+
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
